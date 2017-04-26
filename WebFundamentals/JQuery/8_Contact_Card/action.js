ce8fcb339c06d4b3868f77e1e7a3bb30d16c7425
Mike Hoyt <derukimlh@gmail.com>
2017-04-25 18:31:37 -0700

    HTML at least most complete; CSS complete except for div.card; JS only framed

---
+++ WebFundamentals/JQuery/8_Contact_Card/action.js
@@ -0,0 +1,11 @@
+// -*- javascript -*-
+$(document).ready( function(){
+  $(document).on("click", "#add_user_btn", function(){
+    alert( "You clicked the 'Add User' button!");
+  });
+});
+
+// <div class="card" data-first-name="..." data-last-name="..." data-desc="...">
+//   <h2>FirstName LastName</h2>
+//   <p>Click for description!</p>
+// </div>
