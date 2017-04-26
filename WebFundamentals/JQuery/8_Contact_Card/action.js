7039b6814653cbdf579554c592a8f44ce8f12cdd
Mike Hoyt <derukimlh@gmail.com>
2017-04-25 22:05:09 -0700

    CSS completed (div.card styling added); JS mostly completed (one bug in setting data-state)

--- WebFundamentals/JQuery/8_Contact_Card/action.js
+++ WebFundamentals/JQuery/8_Contact_Card/action.js
@@ -1,11 +1,64 @@
 // -*- javascript -*-
+
+function checkAddUserForm() {
+  var alert_msg = "";
+
+  if( $('#first_name').val() == "" ) {
+    alert_msg += "Invalid empty 'First Name' field!" + "\n";
+  }
+  if( $('#last_name').val() == "" ) {
+    alert_msg += "Invalid empty 'Last Name' field!" + "\n";
+  }
+  if( $('#desc').val() == "" ) {
+    alert_msg += "Invalid empty 'Description' field!" + "\n";
+  }
+
+  if( alert_msg !== "" ) {
+    alert( alert_msg );
+    return( false );
+  }
+  else {
+    return( true );
+  }
+}
+
+function clearAddUserForm() {
+  $('#first_name').val( "" );
+  $('#last_name').val( "" );
+  $('#desc').val( "" );
+}
+
 $(document).ready( function(){
   $(document).on("click", "#add_user_btn", function(){
-    alert( "You clicked the 'Add User' button!");
+    if( checkAddUserForm() ) {
+      $('#sidebar').prepend(
+        "<div class='card'" +
+          " data-first-name='" + $('#first_name').val() + "'" +
+          " data-last-name='" + $('#last_name').val() + "'" +
+          " data-desc='" + $('#desc').val() + "'" +
+          " data-state='" + "name" + "'" +
+          ">" +
+          "<h2>" + $('#first_name').val() + " " + $('#last_name').val() + "</h2>" +
+          "<p>" + "Click for description!" + "</p>" +
+        "</div>"
+      );
+      clearAddUserForm();
+    }
+  });
+
+  $(document).on("click", ".card p", function(){
+    if( $(this).parent().data( "state" ) == "name" ) {
+      $(this).parent().html(
+        "<p>" + $(this).parent().data( "desc" ) + "</p>"
+      );
+      $(this).parent().data( "state", "desc" );
+    }
+    else {
+      $(this).parent().html(
+          "<h2>" + $(this).parent().data( "first-name" ) + " " + $(this).parent().data( "last-name" ) + "</h2>" +
+          "<p>" + "Click for description!" + "</p>"
+      );
+      $(this).parent().data( "state", "name" );
+    }
   });
 });
-
-// <div class="card" data-first-name="..." data-last-name="..." data-desc="...">
-//   <h2>FirstName LastName</h2>
-//   <p>Click for description!</p>
-// </div>
