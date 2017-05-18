d9fa40ec8ce515ef286fe6d1d70825b868969855
Mike Hoyt <derukimlh@gmail.com>
2017-05-18 15:45:03 -0700

    Added 6_Django/_0_Getting_Started work

---
+++ Python/6_Django/_0_Getting_Started/apps/first_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# def index(request):
+#   response = "Hello, I am your first request!"
+#   return HttpResponse( response )
+
+def index(request):
+  print '*' * 100
+  return render( request, 'first_app/index.html' )
+
