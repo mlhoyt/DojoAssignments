dcee1658ae92c3c3166327fe893abd3eb5aa33ed
Mike Hoyt <derukimlh@gmail.com>
2017-05-24 15:35:20 -0700

    Added apps/secrets_app/models.py

--- Python/6_Django/_28_Dojo_Secrets/apps/secrets_app/models.py
+++ Python/6_Django/_28_Dojo_Secrets/apps/secrets_app/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
+from ..login_app.models import Users
 
 from django.db import models
 
-# Create your models here.
+class Secrets( models.Model ):
+    secret = models.CharField( max_length = 255 )
+    authored_by = models.ForeignKey( Users, related_name = "authored_secrets" )
+    liked_by = models.ManyToManyField( Users, related_name = "likes_secrets" )
+    created_at = models.DateTimeField( auto_now_add = True )
+    updated_at = models.DateTimeField( auto_now = True )
