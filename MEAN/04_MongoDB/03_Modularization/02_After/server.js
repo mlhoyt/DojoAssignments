17860ec2449a3bb02d219dbc1fd4e0c485d17aa3
Mike Hoyt <derukimlh@gmail.com>
2017-06-10 05:54:02 -0700

    Moved routes to server/config/routes.js and updated server.js to require .../routes.js

--- MEAN/04_MongoDB/03_Modularization/02_After/server.js
+++ MEAN/04_MongoDB/03_Modularization/02_After/server.js
@@ -19,24 +19,7 @@ app.use(express.static(path.join(__dirname, "./static")));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // root route to render the index.ejs view
-app.get('/', function(req, res) {
-  res.render("index");
-})
-app.post('/quotes', function(req,res){
-  var quote = new Quote({name: req.body.name, quote: req.body.quote});
-  quote.save(function(err) {
-    if(err){
-      console.log("something went wrong");
-    } else {
-      res.redirect('/main');
-    }
-  })
-})
-app.get('/main', function (req, res) {
-  Quote.find({}, function (err, quotes){
-    res.render('main', {quotes:quotes});
-  });
-})
+require( './server/config/routes.js' )( app );
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
   console.log("listening on port 8000");
