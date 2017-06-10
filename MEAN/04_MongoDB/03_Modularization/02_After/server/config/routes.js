4f14f9bf36d9aef816859b02f0320af6a5506805
Mike Hoyt <derukimlh@gmail.com>
2017-06-10 05:59:54 -0700

    Split server/config/routes.js into itself and server/controllers/quotes.js

--- MEAN/04_MongoDB/03_Modularization/02_After/server/config/routes.js
+++ MEAN/04_MongoDB/03_Modularization/02_After/server/config/routes.js
@@ -3,25 +3,18 @@ var mongoose = require('mongoose');
 
 var Quote = mongoose.model('Quote');
 
+var quotes = require('../controllers/quotes.js');
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.render("index");
+    res.render('index')
   })
 
   app.post('/quotes', function(req,res){
-    var quote = new Quote({name: req.body.name, quote: req.body.quote});
-    quote.save(function(err) {
-      if(err){
-        console.log("something went wrong");
-      } else {
-        res.redirect('/main');
-      }
-    })
+    quotes.create(req, res)
   })
 
   app.get('/main', function (req, res) {
-    Quote.find({}, function (err, quotes){
-      res.render('main', {quotes:quotes});
-    });
+    quotes.show(req, res)
   })
 }
