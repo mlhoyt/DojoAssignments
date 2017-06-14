87b9f067effa476e1ae53250070716ee2f284d85
Mike Hoyt <derukimlh@gmail.com>
2017-06-14 06:38:56 -0700

    Removed unnecessary component quote-item

--- MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.module.ts
+++ MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QuoteNewComponent } from './quote-new/quote-new.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
-import { QuoteItemComponent } from './quote-item/quote-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteNewComponent,
-    QuoteListComponent,
-    QuoteItemComponent
+    QuoteListComponent
   ],
   imports: [
     BrowserModule,
