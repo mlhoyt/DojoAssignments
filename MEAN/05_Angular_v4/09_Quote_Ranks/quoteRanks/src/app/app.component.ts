ad22c49dcc3c469b67cb5ac4b2c74a283dc51d00
Mike Hoyt <derukimlh@gmail.com>
2017-06-14 06:23:34 -0700

    Updated app.component HTML/TS: renamed "storeNewQuote" to "createQuote" Updated app.component TS: added "updateQuote" functionality (1=voteUp, 2=voteDown, 3=delete)

--- MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.component.ts
+++ MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.component.ts
@@ -12,24 +12,24 @@ export class AppComponent {
   title = 'app';
 
   constructor() {
-    this.storeNewQuote({
+    this.createQuote({
       text: "I never am really satisfied that I understand anything, because, understand it well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand about the many connections and relations which occur to me.",
       author: "Ada Lovelace",
       votes: 27,
     });
-    this.storeNewQuote({
+    this.createQuote({
       text: "To be, or not to be",
       author: "Prince Hamlet",
       votes: 25,
     });
-    this.storeNewQuote({
+    this.createQuote({
       text: "There are risks and costs to action.  But they are far less than the long range risks of comfortable inaction.",
       author: "John F. Kennedy",
       votes: 7,
     });
   }
 
-  storeNewQuote( eventData ) {
+  createQuote( eventData ) {
     this.quotes.push({
       id: ++this.quote_id,
       text: eventData.text,
@@ -39,6 +39,20 @@ export class AppComponent {
   }
 
   updateQuote( eventData ) {
-    console.log( "Debug: AppComponent: updateQuote: id:", eventData.id, "action:", eventData.action );
+    for( let i = 0; i < this.quotes.length; ++i ) {
+      let quote = this.quotes[ i ];
+      if( quote.id === eventData.id ) {
+        if( eventData.action === 1 ) {
+          quote.votes++;
+        }
+        else if( eventData.action === 2 ) {
+          quote.votes--;
+        }
+        else if( eventData.action === 3 ) {
+          this.quotes.splice( i, 1 );
+        }
+        break;
+      }
+    }
   }
 }
