3d37d35f3cf9dc0398e84180ed185e2ea63c20a4
Mike Hoyt <derukimlh@gmail.com>
2017-06-13 22:29:29 -0700

    Updated app.component.ts: added unique "id" to each object in "quotes" Updated app.component.html: routed app-root ("quotes") to app-quote-list Updated quote-list.component.ts: added input "quotes"

--- MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.component.ts
+++ MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/app.component.ts
@@ -6,12 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  quote_id: number = 0;
   quotes = [];
 
   title = 'app';
 
+  constructor() {
+    this.storeNewQuote({
+      text: "I never am really satisfied that I understand anything, because, understand it well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand about the many connections and relations which occur to me.",
+      author: "Ada Lovelace",
+      votes: 27,
+    });
+    this.storeNewQuote({
+      text: "To be, or not to be",
+      author: "Prince Hamlet",
+      votes: 25,
+    });
+    this.storeNewQuote({
+      text: "There are risks and costs to action.  But they are far less than the long range risks of comfortable inaction.",
+      author: "John F. Kennedy",
+      votes: 7,
+    });
+  }
+
   storeNewQuote( eventData ) {
-    this.quotes.push( { text: eventData.text, author: eventData.author, votes: 0 } );
-    console.log( "Debug: AppComponent: storeNewQuote: quotes:", this.quotes ); // DEBUG
+    this.quotes.push({
+      id: ++this.quote_id,
+      text: eventData.text,
+      author: eventData.author,
+      votes: eventData.votes || 0
+    });
   }
 }
--- MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/quote-list/quote-list.component.ts
+++ MEAN/05_Angular_v4/09_Quote_Ranks/quoteRanks/src/app/quote-list/quote-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-list.component.css']
 })
 export class QuoteListComponent implements OnInit {
-
+  @Input() quotes;
+  
   constructor() { }
 
   ngOnInit() {
