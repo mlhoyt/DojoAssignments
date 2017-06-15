57c84319db4315e41f038961c3c520cde760c33e
Mike Hoyt <derukimlh@gmail.com>
2017-06-15 14:29:08 -0700

    Updated other weather-{{CITY}} TS/HTML/CS to match the working version

--- MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-chicago/weather-chicago.component.ts
+++ MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-chicago/weather-chicago.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-weather-chicago',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-chicago.component.css']
 })
 export class WeatherChicagoComponent implements OnInit {
+  data = [];
 
-  constructor() { }
+  owapi_url = "http://api.openweathermap.org/data/2.5/weather";
+  owapi_uid = "APPID=9e6717fb14947b2948cd7f033dacd57f";
+  owapi_args = "q=chicago";
+
+  constructor(
+    private _httpService: HttpService
+  )
+  {}
 
   ngOnInit() {
+    this._httpService.get( this.owapi_url + "?" + this.owapi_uid + "&" + this.owapi_args )
+      .then( data => { this.data = data; } )
+      .catch( err => { console.log( "Error: WeatherChicagoComponent: HttpService.get:" + err ); } );
   }
 
+  convertTempKToF( tempK ) {
+    return ( 32 + ((tempK - 275) * 9/4) );
+  }
 }
--- MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-sanjose/weather-sanjose.component.ts
+++ MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-sanjose/weather-sanjose.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-weather-sanjose',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-sanjose.component.css']
 })
 export class WeatherSanjoseComponent implements OnInit {
+  data = [];
 
-  constructor() { }
+  owapi_url = "http://api.openweathermap.org/data/2.5/weather";
+  owapi_uid = "APPID=9e6717fb14947b2948cd7f033dacd57f";
+  owapi_args = "q=sanjose";
+
+  constructor(
+    private _httpService: HttpService
+  )
+  {}
 
   ngOnInit() {
+    this._httpService.get( this.owapi_url + "?" + this.owapi_uid + "&" + this.owapi_args )
+      .then( data => { this.data = data; } )
+      .catch( err => { console.log( "Error: WeatherSanjoseComponent: HttpService.get:" + err ); } );
   }
 
+  convertTempKToF( tempK ) {
+    return ( 32 + ((tempK - 275) * 9/4) );
+  }
 }
