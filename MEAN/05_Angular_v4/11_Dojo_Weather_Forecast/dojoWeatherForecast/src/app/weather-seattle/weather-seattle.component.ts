0cf23828a0b52417ef0ae05800058d04425dc0a6
Mike Hoyt <derukimlh@gmail.com>
2017-06-15 13:53:41 -0700

    Updated weather-seattle.component content

--- MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-seattle/weather-seattle.component.ts
+++ MEAN/05_Angular_v4/11_Dojo_Weather_Forecast/dojoWeatherForecast/src/app/weather-seattle/weather-seattle.component.ts
@@ -7,6 +7,11 @@ import { HttpService } from '../http.service';
   styleUrls: ['./weather-seattle.component.css']
 })
 export class WeatherSeattleComponent implements OnInit {
+  data = [];
+
+  owapi_url = "http://api.openweathermap.org/data/2.5/weather";
+  owapi_uid = "APPID=9e6717fb14947b2948cd7f033dacd57f";
+  owapi_args = "q=seattle";
 
   constructor(
     private _httpService: HttpService
@@ -14,6 +19,12 @@ export class WeatherSeattleComponent implements OnInit {
   {}
 
   ngOnInit() {
+    this._httpService.get( this.owapi_url + "?" + this.owapi_uid + "&" + this.owapi_args )
+      .then( data => { this.data = data; } )
+      .catch( err => { console.log( "Error: WeatherSeattleComponent: HttpService.get:" + err ); } );
   }
 
+  convertTempKToF( tempK ) {
+    return ( 32 + ((tempK - 275) * 9/4) );
+  }
 }
