842535839d6de295d81428cfd9a9481b396076fa
Mike Hoyt <derukimlh@gmail.com>
2017-06-17 20:47:39 -0700

    Moved notes data structure from list-notes to notes and passed down

--- MEAN/06_Full_MEAN/01_Anonymous_Notes/client/src/app/notes/notes.component.ts
+++ MEAN/06_Full_MEAN/01_Anonymous_Notes/client/src/app/notes/notes.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notes = [
+    { _id: "1", text: "The new coffee shop has really good cold brew!", createdAt: "2017-06-11T06:11:58.001Z" },
+    { _id: "2", text: "10,000 hours to mastery, we get over 1,500 hours of coding experience at Coding Dojo in less than 4 months.  Think about it.", createdAt: "2017-05-23T09:23:13.001Z" },
+    { _id: "3", text: "Programming makes the day go by so fast, and it's so much fun!", createdAt: "2017-05-23T09:23:13.001Z" },
+    { _id: "4", text: "One more to test the overflow", createdAt: "2017-06-17T19:41:13.241Z" },
+  ]
 
   constructor() { }
 
