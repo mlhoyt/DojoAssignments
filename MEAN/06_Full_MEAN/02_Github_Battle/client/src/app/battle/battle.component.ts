de1f55533a63ec2103cf468640456224353ed15c
Mike Hoyt <derukimlh@gmail.com>
2017-06-19 10:20:11 -0700

    Removed passed events: Moved github-api.service access to select.component; Copied/Moved player-api.service from battle.component to select.component

--- MEAN/06_Full_MEAN/02_Github_Battle/client/src/app/battle/battle.component.ts
+++ MEAN/06_Full_MEAN/02_Github_Battle/client/src/app/battle/battle.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../player';
 import { PlayerApiService } from '../player-api.service';
-import { GithubApiService } from '../github-api.service';
 import { Player1DataService } from '../player1-data.service';
 import { Player2DataService } from '../player2-data.service';
 import { AllPlayersDataService } from '../all-players-data.service';
@@ -18,7 +17,6 @@ export class BattleComponent implements OnInit {
 
   constructor(
     private _playerApi: PlayerApiService,
-    private _githubApi: GithubApiService,
     private _player1DataService: Player1DataService,
     private _player2DataService: Player2DataService,
     private _allPlayersDataService: AllPlayersDataService,
@@ -39,43 +37,5 @@ export class BattleComponent implements OnInit {
         this.all_players = data;
         this._allPlayersDataService.subject.next( this.all_players );
       });
-  }
-
-  have_player( player: Player ) {
-    for( let i = 0; i < this.all_players.length; ++i ) {
-      if( this.all_players[ i ].username === player.username ) {
-        player.avatar_url = this.all_players[ i ].avatar_url;
-        player.score = this.all_players[ i ].score;
-        player.isInvalid = false;
-        player.isLoaded = true;
-        return( true );
-      }
-    }
-    return( false );
-  }
-
-  get_user( player ) {
-    if( ! this.have_player( player ) ) {
-      this._githubApi.read_user( player.username )
-        .catch( err => {
-          console.log( "Error: AppBattleComponent: get_user:", player.username, "not found!" );
-          player.avatar_url = "";
-          player.score = 0;
-          player.isInvalid = true;
-          player.isLoaded = false;
-        })
-        .then( data => {
-          if( ! player.isInvalid ) {
-            player.avatar_url = data.avatar_url;
-            player.score = (data.followers + data.public_repos) * 12;
-            player.isInvalid = false;
-            player.isLoaded = true;
-
-            this._playerApi.create( player )
-              .catch( err => { console.log( "Error: AppBattleComponent: get_user: create_player:", err ); } )
-              .then( () => { this.get_all_players(); } );
-          }
-        });
-    }
   }
 }
